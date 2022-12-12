17460a450f1971c75502010ccd64f316d8e5ad84
Vincenzo Petrone <vipetrone@unisa.it>
2022-12-12 17:10:01 +0100

    [FIX] Fixes the evaluation of c2(x) + s2(x)

--- Scheme/ex01_evaluate-series.scm
+++ Scheme/ex01_evaluate-series.scm
@@ -44,14 +44,6 @@
 ; evaluate-series ;
 ; --------------- ;
 
-; Returns a stream of integers starting from n
-(define (integers-from n)
-  (stream-cons n (integers-from (+ n 1))))
-
-; Returns a stream of the powers of x, i.e. x^n, x^{n+1}, x^{n+2}, ...
-(define (powers-from n x)
-  (stream-map2r expt x (integers-from n)))
-
 ; Returns a stream of powers of x, computed according to an exponent function
 (define (powers exp-func x n)
   (stream-cons (expt x (exp-func n))
@@ -88,13 +80,8 @@
 ; ---------- ;
 
 ; Returns a stream representing the series S(x) = S1(x) + S2(x),
-; and exp-func is a function returning the exponent function of x
-(define (add-power-series exp-func S1 S2 x)
-  (evaluate-power-series exp-func (+-streams S1 S2) x))
-
-; Returns a stream representing the series S(x) = S1(x) + S2(x)
-(define (add-series S1 S2 x)
-  (add-power-series exp-exp-func S1 S2 x))
+(define (add-series S1 S2)
+  (+-streams S1 S2))
 
 ; --------------- ;
 ; multiply-series ;
@@ -108,20 +95,13 @@
 
 ; Returns a stream represeting the series S(x) = S1(x) * S2(x)
 ; It uses the theoretical result that S(x) is the power series evaluated at x,
-; where the coefficients of S are the cachy products between S1 and S2,
-; and exp-func is a function returning the exponent function of x
-(define (multiply-power-series exp-func S1 S2 x)
-  (define (one n) 1)
-  (define (multiply-power-series-sub exp-func S1 S2 n)
-    (stream-cons (stream-ref (evaluate-power-series one (cauchy-product S1 S2 n) 1) n)
-                 (multiply-power-series-sub exp-func S1 S2 (+ n 1))))
-  (evaluate-power-series exp-func (multiply-power-series-sub exp-func S1 S2 0) x))
-
-; Returns a stream represeting the series S(x) = S1(x) * S2(x)
-; It uses the theoretical result that S(x) is the power series evaluated at x,
 ; where the coefficients of S are the cachy products between S1 and S2
-(define (multiply-series S1 S2 x)
-  (multiply-power-series exp-exp-func S1 S2 x))
+(define (multiply-series S1 S2)
+  (define (one n) 1)
+  (define (multiply-series-sub S1 S2 n)
+    (stream-cons (stream-ref (evaluate-power-series one (cauchy-product S1 S2 n) 1) n)
+                 (multiply-series-sub S1 S2 (+ n 1))))
+  (multiply-series-sub S1 S2 0))
 
 ; ---------------- ;
 ; integrate-series ;
@@ -130,7 +110,6 @@
 ; Returns a stream containing the coeffients of the series
 ; representing the integral of the series S(x),
 ; where S is the stream of the coefficients of the series,
-; and exp-func is a function returning the exponent function of x
 (define (integrate-power-series S exp-func)
   (define (exp-stream n)
     (stream-cons (+ (exp-func n) 1)
@@ -147,25 +126,31 @@
 ; trigonometric-series ;
 ; -------------------- ;
 
-; 2n
-(define (cos-exp-func n)
-  (* 2 n))
+; returns x % 2
+(define (parity x)
+  (modulo x 2))
 
-; 2n + 1
-(define (sin-exp-func n)
-  (+ (* 2 n) 1))
-
-; Returns the stream of the coefficients of cosine expressed as power series
-; a_n = (-1)^n / (2n)!
+; Returns the stream of the coefficients of cosine expressed as Taylor series
 (define (cos-coeffs-sub n)
-  (stream-cons (/ (expt -1 n) (stream-ref facts (* 2 n)))
+  (define (cos-sign n)
+    (if (even? n)
+        (if (even? (/ n 2))
+            +1
+            -1)
+        0))
+  (stream-cons (* (/ (expt -1 n) (stream-ref facts n)) (- 1 (parity n)) (cos-sign n))
                (cos-coeffs-sub (+ n 1))))
 (define cos-coeffs (cos-coeffs-sub 0))
 
-; Returns the stream of the coefficients of sine expressed as power series
-; a_n = (-1)^n / (2n + 1)!
+; Returns the stream of the coefficients of sine expressed as Taylor series
 (define (sin-coeffs-sub n)
-  (stream-cons (/ (expt -1 n) (stream-ref facts (+ (* 2 n) 1)))
+  (define (sin-sign n)
+    (if (even? n)
+        0
+        (if (even? (/ (+ n 1) 2))
+            +1
+            -1)))
+  (stream-cons (* (/ (expt -1 n) (stream-ref facts n)) (parity n) (sin-sign n))
                (sin-coeffs-sub (+ n 1))))
 (define sin-coeffs (sin-coeffs-sub 0))
 
@@ -173,16 +158,31 @@
 ; as the integral of the sine series
 (define cos-series
   (stream-cons 1
-               (stream-map2r - 0 (integrate-power-series sin-coeffs sin-exp-func))))
+               (stream-map2r - 0 (integrate-power-series sin-coeffs exp-exp-func))))
 
 ; Returns the stream of the coefficients of sine expressed
 ; as the integral of the cosine series
 (define sin-series
-  (integrate-power-series cos-coeffs cos-exp-func))
+  (stream-cons 0
+               (integrate-power-series cos-coeffs exp-exp-func)))
+
+; Returns a stream containing only the elements in stream s at even positions
+(define (only-even s)
+  (stream-cons (stream-first s)
+               (only-even (stream-rest (stream-rest s)))))
+
+; Returns a stream containing only the elements in stream s at odd positions
+(define (only-odd s)
+  (stream-cons (stream-first (stream-rest s))
+               (only-odd (stream-rest (stream-rest s)))))
 
 ; ---- ;
 ; MAIN ;
 ; ---- ;
+
+; Returns a stream of integers starting from n, i.e. n, n+1, n+2, ...
+(define (integers-from n)
+  (stream-cons n (integers-from (+ n 1))))
 
 ; Returns a stream of factorials, i.e. 1, 1, 2, 6, 24, ...
 (define facts
@@ -193,7 +193,7 @@
   (stream-map2l expt facts -1))
 
 ; e^x
-(define exponent 3)
+(define exponent 1)
 
 ; Define e (the Euler number) as a stream
 (define e-stream (evaluate-series inv-facts exponent))
@@ -203,72 +203,100 @@
 (define tolerance (expt 10 (- 0 decimal-tolerance)))
 (define e (within tolerance e-stream))
 (define e-string (real->decimal-string e decimal-tolerance))
-(printf "* The appoximation of e^~a up to ~a decimal points is\n~a\n\n"
+(printf "* The appoximation of e^~a up to ~a decimal points is\n  ~a\n\n"
         exponent decimal-tolerance e-string)
 
 ; Get the n-th approximation of e
 (define n 10)
 (define e-approx (exact->inexact (stream-ref e-stream n)))
 (define e-approx-string (real->decimal-string e-approx decimal-tolerance))
-(printf "* The ~a-th approximation of e^~a is\n~a\n\n"
+(printf "* The ~a-th approximation of e^~a is\n  ~a\n\n"
         n exponent e-approx-string)
 
-; Approximate 2e with a stream (test add-series)
-(define 2*e-stream (add-series inv-facts inv-facts exponent))
-(define 2*e (within tolerance 2*e-stream))
+; Approximate 2e with a stream (tests add-series)
+(define 2*e-stream (add-series inv-facts inv-facts))
+(define 2*e (within tolerance (evaluate-series 2*e-stream exponent)))
 (define 2*e-string (real->decimal-string 2*e decimal-tolerance))
-(printf "* The appoximation of 2e^~a up to ~a decimal points is\n~a\nThe actual value is\n~a\n\n"
+(printf "* The appoximation of 2e^~a up to ~a decimal points is\n  ~a\n  The actual value is\n  ~a\n\n"
         exponent decimal-tolerance 2*e-string
         (real->decimal-string (within tolerance (stream-map2r * 2 e-stream)) decimal-tolerance))
 
-; Approximate e^2 with a stream (test multiply-series)
-(define e2-stream (multiply-series inv-facts inv-facts exponent))
-(define e2 (within tolerance e2-stream))
+; Approximate e^2 with a stream (tests multiply-series)
+(define e2-stream (multiply-series inv-facts inv-facts))
+(define e2 (within tolerance (evaluate-series e2-stream exponent)))
 (define e2-string (real->decimal-string e2 decimal-tolerance))
-(printf "* The appoximation of e^~a up to ~a decimal points is\n~a\nThe actual value is\n~a\n\n"
+(printf "* The appoximation of e^~a up to ~a decimal points is\n  ~a\n  The actual value is\n  ~a\n\n"
         (* 2 exponent) decimal-tolerance e2-string
         (real->decimal-string (within tolerance (*-streams e-stream e-stream)) decimal-tolerance))
 
-; Approximate e^x with its integral
+; Approximate e^x with its integral (test integrate-series)
 (define exp-series
   (stream-cons 1 (integrate-series exp-series)))
 (define exp (within tolerance (evaluate-series exp-series exponent)))
 (define exp-string (real->decimal-string exp decimal-tolerance))
-(printf "* The integral of e^x (evaluated at x=~a) up to ~a decimal points is\n~a\nThe actual value is\n~a\n\n"
+(printf "* The integral of e^x (evaluated at x=~a) up to ~a decimal points is\n  ~a\n  The actual value is\n  ~a\n\n"
         exponent decimal-tolerance exp-string
         (real->decimal-string (within tolerance (evaluate-series inv-facts exponent)) decimal-tolerance))
 
 ; ---------------------------- ;
-; Evaluate sin(x)^2 + cos(x)^2 ;
+; Evaluate cos(x)^2 + sin(x)^2 ;
 ; ---------------------------- ;
 
-; First, evaluate with the known coefficients...
-(define x (random))
-(define c (within tolerance (evaluate-power-series cos-exp-func cos-coeffs x)))
-(define s (within tolerance (evaluate-power-series sin-exp-func sin-coeffs x)))
-(define id (+ (expt c 2) (expt s 2)))
+; Generate a random number x in range ] -pi ; +pi [
+(define x (+ (- 0 pi) (* (* 2 pi) (random))))
+(define precision 4)
 
-(define x-string (real->decimal-string x 2))
-(define c-string (real->decimal-string c 2))
-(define s-string (real->decimal-string s 2))
-(define id-string (real->decimal-string id 2))
-(printf "* cos(~a) = ~a, sin(~a) = ~a,\n  cos(~a)^2 + sin(~a)^2 = ~a\n"
-        x-string c-string
-        x-string s-string
+; First, evaluate with the known coefficients...
+(define c (within tolerance (only-odd (evaluate-series cos-coeffs x))))
+(define s (within tolerance (only-even (evaluate-series sin-coeffs x))))
+(define c2 (expt c 2))
+(define s2 (expt s 2))
+(define id (+ c2 s2))
+
+(define x-string (real->decimal-string x precision))
+(define c-string (real->decimal-string c precision))
+(define s-string (real->decimal-string s precision))
+(define c2-string (real->decimal-string c2 precision))
+(define s2-string (real->decimal-string s2 precision))
+(define id-string (real->decimal-string id precision))
+(printf "* Evaluating the trigonometric identity cos(x)^2 + sin(x)^2 for x=~a...\n  cos(~a) = ~a, sin(~a) = ~a,\n  cos(~a)^2 = ~a, sin(~a)^2 = ~a\n  cos(~a)^2 + sin(~a)^2 = ~a\n"
+        x-string
+        x-string c-string x-string s-string
+        x-string c2-string x-string s2-string
         x-string x-string id-string)
 
 ; ...Then, evaluate with the coefficients computed from the integrals...
-(define c-int (within tolerance (evaluate-power-series cos-exp-func cos-series x)))
-(define s-int (within tolerance (evaluate-power-series sin-exp-func sin-series x)))
-(define id-int (+ (expt c-int 2) (expt s-int 2)))
+(define c-int (within tolerance (only-even (evaluate-series cos-series x))))
+(define s-int (within tolerance (only-odd (evaluate-series sin-series x))))
+(define c2-int (expt c-int 2))
+(define s2-int (expt s-int 2))
+(define id-int (+ c2-int s2-int))
 
-(define c-int-string (real->decimal-string c-int 2))
-(define s-int-string (real->decimal-string s-int 2))
-(define id-int-string (real->decimal-string id-int 2))
-(printf "* After integation, cos(~a) = ~a, sin(~a) = ~a,\n  cos(~a)^2 + sin(~a)^2 = ~a\n"
-        x-string c-int-string
-        x-string s-int-string
+(define c-int-string (real->decimal-string c-int precision))
+(define s-int-string (real->decimal-string s-int precision))
+(define c2-int-string (real->decimal-string c2-int precision))
+(define s2-int-string (real->decimal-string s2-int precision))
+(define id-int-string (real->decimal-string id-int precision))
+(printf "* After integration...\n  cos(~a) = ~a, sin(~a) = ~a,\n  cos(~a)^2 = ~a, sin(~a)^2 = ~a\n  cos(~a)^2 + sin(~a)^2 = ~a\n"
+        x-string c-int-string x-string s-int-string
+        x-string c2-int-string x-string s2-int-string
         x-string x-string id-int-string)
 
-; ...GRAN FINALE
-;(define id-stream (add-series (multiply-series 
+; ----------------------------------------------- ;
+; GRAN FINALE (tests all the developed functions) ;
+; ----------------------------------------------- ;
+
+(define c2-stream (multiply-series cos-series cos-series))
+(define s2-stream (multiply-series sin-series sin-series))
+(define c2-mul (within tolerance (only-even (evaluate-series c2-stream x))))
+(define s2-mul (within tolerance (only-odd (evaluate-series s2-stream x))))
+(define c2+s2-stream (add-series c2-stream s2-stream))
+(define id-add (within tolerance (evaluate-series c2+s2-stream x)))
+
+(define c2-mul-string (real->decimal-string c2-mul precision))
+(define s2-mul-string (real->decimal-string s2-mul precision))
+(define id-add-string (real->decimal-string id-add precision))
+(printf "* Approximating cos(x)^2 with (cos*cos)(x), sin(x)^2 with (sin*sin)(x), and cos(x)^2 + sin(x)^2 with ((cos*cos)+(sin*sin))(x)...\n  cos(~a) = ~a, sin(~a) = ~a,\n  cos(~a)^2 = ~a, sin(~a)^2 = ~a\n  cos(~a)^2 + sin(~a)^2 = ~a\n"
+        x-string c-int-string x-string s-int-string
+        x-string c2-int-string x-string s2-int-string
+        x-string x-string id-int-string)
